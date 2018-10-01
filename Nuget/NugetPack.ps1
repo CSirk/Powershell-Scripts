b7a13ce11df2472f12b6d31e07a300545c6207c4
Cody Sirk <csirk005@gmail.com>
2018-10-01 00:09:08 -0500

    initial commit

---
+++ Nuget/NugetPack.ps1
@@ -0,0 +1,8 @@
+﻿$projectPath = "";
+$nugetExecutablePath = "\" + "nuget.exe";
+$nuspecFile = ".nuspec";
+$version = "1.0.0";
+
+
+cd $projectPath;
+&$nugetExecutablePath pack $nuspecFile -Version $version;
