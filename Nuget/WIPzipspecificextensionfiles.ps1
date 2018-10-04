ea53fbd09f876747891c5f514dbed85087d3282a
Cody Sirk <csirk005@gmail.com>
2018-10-03 23:22:55 -0500

    added wip script

---
+++ Nuget/WIPzipspecificextensionfiles.ps1
@@ -0,0 +1,18 @@
+﻿$folderPathForCompression = "c:\users\codas\desktop\TestCompress\";
+$destinationZipPath = "c:\users\codas\desktop\TestCompress" + "\test.zip";
+$filesToZip = (Get-ChildItem -Path $folderPathForCompression -Include *.dll, *.exe);
+
+#$filesPaths = "";
+
+#foreach($test in $filesToZip)
+#{
+#$filesPaths = $filesPaths + ", " + $test
+#}
+
+#$filesPaths = $filesPaths.Remove(0, 2);
+
+#Write-Host $filesPaths;
+
+#Compress-Archive -Path $folderPathForCompression -CompressionLevel Optimal -DestinationPath $destinationZipPath
+
+Get-ChildItem -Path $folderPathForCompression -Include *.dll, *.exe | Compress-Archive -DestinationPath $destinationZipPath -Update
