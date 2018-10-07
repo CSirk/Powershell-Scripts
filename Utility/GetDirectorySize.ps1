e49d802f64de784804f19d4ebb18815a0e1b5105
Cody Sirk <csirk005@gmail.com>
2018-10-07 00:18:19 -0500

    added get directory size

---
+++ Utility/GetDirectorySize.ps1
@@ -0,0 +1,3 @@
+﻿$directoryToQuery = "C:\users\codas\Dropbox\programming\Git\CSharp";
+
+"{0:N2}" -f ((Get-ChildItem -path $directoryToQuery -recurse | Measure-Object -property length -sum ).sum /1MB) + " MB"
