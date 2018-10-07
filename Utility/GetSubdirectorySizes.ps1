13b5ad0d51518b835e96829ad7e493fe6bc12578
Cody Sirk <csirk005@gmail.com>
2018-10-07 01:22:13 -0500

    added getsubdirectorysizes

---
+++ Utility/GetSubdirectorySizes.ps1
@@ -0,0 +1,9 @@
+﻿#Get subdirectory sizes and display them in MB in order of size descending 
+$directoryToQuery = "C:\users\codas\Dropbox";
+
+Get-ChildItem $directoryToQuery -Recurse -Directory | ForEach-Object{
+    [pscustomobject]@{
+        Folder = $_.FullName
+        SizeInMB = [math]::Round(((Get-ChildItem -path $_.FullName -recurse | Measure-Object -property length -sum ).sum /1MB), 3);
+    }
+} | Select-Object Folder, SizeInMB | Sort-Object -Property SizeInMB -Descending
